e3d1a5eef84cf0bbf3acf1807f6a8bacd712aed3
AH <ahn.nayeem@gmail.com>
2018-12-01 13:32:08 +0600

    Update chat design

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -18,10 +18,49 @@ function showRightSection(type){
 	}
 }
 
+function closeRighSection(type){
+	if(type == 'chatPage'){
+		$('#chatContainer').hide();
+		$('#dashBoardHead').show();
+		$('body').css('overflow-y', 'auto');
+	}
+}
+
 function startConversation(convID){
 	$('#dashBoardHead').hide();
 	$('#allUserTable').hide();
 	$('#chatContainer').show();
+	$('body').css('overflow', 'hidden');
+	$('.chat-block').html('');
 	// console.log(id);
+	$('#writeMsgInput').focus();
+
+}
+
+function drawMsg (e){
+	var msgValue = $('#writeMsgInput').val();
+
+	var html = '<div class="row">';
+	html += '<div class="col-sm-1">';
+	html += '<img class="img-avatar img-avatar-48" src="/usersImg/dalimchy.jpg" alt="User profile pic">';
+	html += '</div>';
+	html += '<div class="col-sm-11">';
+	html += '<div class="row">';
+	html += '<h3 class="msg-sendar-name">AH Nayeem</h3>';
+	html += '<p>'+msgValue+'</p>';
+	html += '</div>';
+	html += '</div>';
+	html += '</div>';
+
+	var keyCode = 'which' in e ? e.which : e.keyCode;
+	if(keyCode == 13){
+		if($('#writeMsgInput').val() !== ''){
+
+			$('.chat-block').append(html);
+			$('#writeMsgInput').val('');
+			var scrollHeight = $(".chat-block").get(0).scrollHeight;
+			$(".chat-block").animate({ scrollTop: scrollHeight });
+		}
+	}
 
 }
